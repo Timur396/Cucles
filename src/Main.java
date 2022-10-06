e2cc61f02933f9905bd1417d34429327926c8630
Аминов <aminov-timur92@mail.ru>
2022-10-06 17:54:28 +0400

    Циклы

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+        // Задание 1.1
+        for (int i = 1; i <= 10; i++) {
+            //System.out.println(i);
+        }
+
+        //Задание 2.1
+        for (int i = 10; i >= 1; i--) {
+            //System.out.println(i);
+        }
+
+        //Задание 3.1
+        for (int i = 0; i <= 17; i = i + 2) {
+            //System.out.println(i);
+        }
+
+
+        for (int i = 10; i >= -10; i = i - 1) {
+            // System.out.println(i);
+        }
+
+        //Заданий 1.2
+        for (int j = 1904; j <= 2096; j = j + 4) {
+            // System.out.println(j);
+        }
+
+        //Заданий 2.2
+        for (int j = 0; j <= 98; j = j + 7) {
+            //System.out.println(j);
+        }
+
+        //Заданий 3.2
+        for (int j = 1; j <= 512; j = j * 2) {
+            // System.out.println(j);
+        }
+
+    }
+}
